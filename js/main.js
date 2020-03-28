e030ff1b70dee719520170255f1c8c8a1f8a347b
Molly Seeley <mseeley89@gmail.com>
2020-03-27 23:33:42 -0400

    smooth scrolling

--- js/main.js
+++ js/main.js
@@ -3,7 +3,20 @@ function initMap() {
     lat: 40.6602037,
     lng: -73.9689,
   }
-  // const frame = { zoom: 14, center: loc }
-  const map = new google.maps.Map(document.querySelector('.map'), { center: loc, zoom: 14 })
-  const marker = new google.maps.Marker({ position: loc, map: map })
+  const frame = { zoom: 14, center: loc };
+  const map = new google.maps.Map(document.querySelector('.map'), frame);
+  const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+$('#navbar a, btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+})
