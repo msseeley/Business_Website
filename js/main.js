ebba6288d7f5ef57e19c6ae6ea1ace9ce14cc7c2
Molly Seeley <mseeley89@gmail.com>
2020-03-27 23:36:41 -0400

    tweak scrollTop distance

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $('#navbar a, btn').on('click', function (event) {
     const hash = this.hash;
     $('html, body').animate(
       {
-        scrollTop: $(hash).offset().top - 100
+        scrollTop: $(hash).offset().top - 60
       },
       800
     );
